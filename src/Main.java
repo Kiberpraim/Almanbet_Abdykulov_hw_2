61bbdbd8b211f48777858198998e7fdec22dbcbb
Kiberpraim <Abdykulovalmanbet4@gmail.com>
2023-03-04 14:06:27 +0600

    Removed the duplicate.

--- src/Main.java
+++ src/Main.java
@@ -46,14 +46,8 @@ public class Main {
     }
 
     public static void test(int age, int temperature) {
-        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
-            System.out.println("Можно идти гулять");
-        } else if (age < 20 && temperature >= 0 && temperature <= 28) {
-            System.out.println("Можно идти гулять");
-        } else if (age > 45 && temperature >= -10 && temperature <= 25) {
-            System.out.println("Можно идти гулять");
-        } else {
-            System.out.println("Оставайтесь дома");
-        }
+        String action = GoForAWalk(age, temperature);
+        System.out.println (action);
+
     }
 }
